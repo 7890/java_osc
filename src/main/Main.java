37b374f02a9d18b0e9d438468f4f42ea3a19fb6f
Thomas Brand <tom@trellis.ch>
2016-04-12 23:43:23 +0200

    move help text, only display with -h

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,12 +2,6 @@ class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("This jar file can be used as a library (in the Java classpath) that provides com.illposed.osc.*.\n");
-		System.out.println("However it also contains classes with a main method");
-		System.out.println("(such as this help text, used as default main class when started with java -jar).\n");
-		System.out.println("Use oscsend: java -cp <path to this jar> oscsend <oscsend args> ...\n");
-		System.out.println("Use oscdump: java -cp <path to this jar> oscdump <oscdump args> ...\n");
-
 		//default to start
 		new OSCGui(args);
 	}
--- src/main/OSCGui.java
+++ src/main/OSCGui.java
@@ -153,6 +153,11 @@ public class OSCGui
 			|| args.length>2
 		)
 		{
+			System.out.println("This jar file can be used as a library (in the Java classpath) that provides com.illposed.osc.*.\n");
+			System.out.println("However it also contains classes with a main method");
+			System.out.println("(such as this help text, used as default main class when started with java -jar).\n");
+			System.out.println("Use oscsend: java -cp <path to this jar> oscsend <oscsend args> ...\n");
+			System.out.println("Use oscdump: java -cp <path to this jar> oscdump <oscdump args> ...\n");
 			System.out.println("Use OSCGui: java -jar <path to this jar>");
 			System.err.println("Syntax: -c (config file)");
 			System.err.println("Example: -c my.properties\n");
