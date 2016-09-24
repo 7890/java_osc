1f79d154ae3b946f9b9d6ae962b982ddc4e4d0d7
Thomas Brand <tom@trellis.ch>
2016-09-24 14:33:58 +0200

    packed bundle

---
+++ src/main/com/illposed/osc/OSCPackBundle.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2016, T. Brand <tom@trellis.ch>
+ * All rights reserved.
+ *
+ * This code is licensed under the BSD 3-Clause license.
+ * See file LICENSE (or LICENSE.html) for more information.
+ */
+
+package com.illposed.osc;
+
+import com.illposed.osc.utility.JavaToByteArrayConverter;
+import com.illposed.osc.utility.OSCJavaToOSCPackByteArrayConverter;
+import java.util.Collection;
+import java.util.Date;
+
+public class OSCPackBundle extends OSCBundle {
+
+	public OSCPackBundle() {
+		super(TIMESTAMP_IMMEDIATE);
+	}
+
+	public OSCPackBundle(Date timestamp) {
+		super(null, timestamp);
+	}
+
+	public OSCPackBundle(Collection<OSCPacket> packets) {
+		super(packets, TIMESTAMP_IMMEDIATE);
+	}
+
+	public OSCPackBundle(Collection<OSCPacket> packets, Date timestamp) {
+		super(packets, timestamp);
+	}
+
+	//implement abstract method from abstract superclass
+	public JavaToByteArrayConverter getConverter()
+	{
+		final JavaToByteArrayConverter stream=new OSCJavaToOSCPackByteArrayConverter();
+		return stream;
+	}
+}//end class OSCPackBundle
+//EOF
