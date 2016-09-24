468d2cce4a2009cecbc534a097b484d671fa43ca
Thomas Brand <tom@trellis.ch>
2016-09-24 06:01:20 +0200

    move readTimeTag() here

--- src/main/com/illposed/osc/utility/AbstractByteArrayToJavaConverter.java
+++ src/main/com/illposed/osc/utility/AbstractByteArrayToJavaConverter.java
@@ -10,7 +10,17 @@
 package com.illposed.osc.utility;
 
 import com.illposed.osc.OSCPacket;
+import com.illposed.osc.OSCBundle;
+
 import java.nio.charset.Charset;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 
 /**
  * Abstract utility class implementing ByteArrayToJavaConverter to convert a byte array,
@@ -71,5 +81,81 @@ public abstract class AbstractByteArrayToJavaConverter implements ByteArrayToJav
 
 	//have a way to know where the packet came from
 	public abstract OSCPacket convert(byte[] bytes, int bytesLength, String remoteHost, int remotePort);
+
+	/**
+	 * Reads the time tag and convert it to a Java Date object.
+	 * A timestamp is a 64 bit number representing the time in NTP format.
+	 * The first 32 bits are seconds since 1900, the second 32 bits are
+	 * fractions of a second.
+	 * @return a {@link Date}
+	 */
+	protected Date readTimeTag(long t) {
+
+		try
+		{
+			ByteBuffer buffer = ByteBuffer.allocate(8);//Long.BYTES);
+			buffer.order(ByteOrder.BIG_ENDIAN);
+			buffer.putLong(t);
+			byte[] bytes=buffer.array();
+			int index=0;
+
+			final byte[] secondBytes = new byte[8];
+			final byte[] fractionBytes = new byte[8];
+			for (int bi = 0; bi < 4; bi++) {
+				// clear the higher order 4 bytes
+				secondBytes[bi] = 0;
+				fractionBytes[bi] = 0;
+			}
+			// while reading in the seconds & fraction, check if
+			// this timetag has immediate semantics
+			boolean isImmediate = true;
+			for (int bi = 4; bi < 8; bi++) {
+				secondBytes[bi] = bytes[index]; index++;
+
+				if (secondBytes[bi] > 0) {
+					isImmediate = false;
+				}
+			}
+			for (int bi = 4; bi < 8; bi++) {
+				fractionBytes[bi] = bytes[index]; index++;
+
+				if (bi < 7) {
+					if (fractionBytes[bi] > 0) {
+						isImmediate = false;
+					}
+				} else {
+					if (fractionBytes[bi] > 1) {
+						isImmediate = false;
+					}
+				}
+			}
+
+			if (isImmediate) {
+
+				return OSCBundle.TIMESTAMP_IMMEDIATE;
+			}
+
+			final long secsSince1900 = new BigInteger(secondBytes).longValue();
+			long secsSince1970 = secsSince1900 - OSCBundle.SECONDS_FROM_1900_TO_1970;
+
+			// no point maintaining times in the distant past
+			if (secsSince1970 < 0) {
+				secsSince1970 = 0;
+			}
+			long fraction = new BigInteger(fractionBytes).longValue();
+
+			// this line was cribbed from jakarta commons-net's NTP TimeStamp code
+			fraction = (fraction * 1000) / 0x100000000L;
+
+			// I do not know where, but I'm losing 1ms somewhere...
+			fraction = (fraction > 0) ? fraction + 1 : 0;
+			final long millisecs = (secsSince1970 * 1000) + fraction;
+			return new Date(millisecs);
+
+		}
+		catch(Exception e)
+		{e.printStackTrace();} ///ev. throw runtime exception
+		return null; //new Date();///
+	}//end readTimeTag()
 } //end abstract class AbstractByteArrayToJavaConverter
 //EOF
