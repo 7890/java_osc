17d197d3a1110496b0ed769e47242841026b6814
Thomas Brand <tom@trellis.ch>
2019-01-27 16:31:09 +0100

    travis: add junit4

--- .ci/install-deps.sh
+++ .ci/install-deps.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 # autotools, automake, make are present in the trusty image
   sudo apt-get install -y \
-    openjdk-7-jdk
+    openjdk-7-jdk \
+    junit4
 fi
 
 exit 0
